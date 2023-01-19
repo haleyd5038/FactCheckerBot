cc90ebd13ba29a423f0bd4b1627dbee4eec1a7be
haley <haleyd1093@gmail.com>
2023-01-18 22:44:31 -0600

    Working out the following issue: SLF4J: Failed to load class "org.slf4j.impl.StaticLoggerBinder". SLF4J: Defaulting to no-operation (NOP) logger implementation SLF4J: See http://www.slf4j.org/codes.html#StaticLoggerBinder for further details. SLF4J: Failed to load class "org.slf4j.impl.StaticMDCBinder". SLF4J: Defaulting to no-operation MDCAdapter implementation. SLF4J: See http://www.slf4j.org/codes.html#no_static_mdc_binder for further details.

--- src/main/java/FactCheckBot.java
+++ src/main/java/FactCheckBot.java
@@ -5,36 +5,51 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import okhttp3.*;
 
 import java.io.IOException;
+import java.net.URL;
 
 public class FactCheckBot extends ListenerAdapter {
-    public static void main(String[] args) throws Exception {
-        JDABuilder builder = JDABuilder.createDefault("bot_token");
-        JDA jda = builder.build();
-        jda.addEventListener(new FactCheckBot());
-    }
+//    public static void main(String[] args) throws Exception {
+//        JDABuilder builder = JDABuilder.createDefault("MTA2MzkyMjc3NDc1OTk3NzA2MA.G523mq.j5RR3ptsx5VOtrryrlTLvJ5QebT1jkA-d1f-xI");
+//        JDA jda = builder.build();
+//        jda.addEventListener(new FactCheckBot());
+//    }
+//
+//    public void onMessageRecieved(MessageReceivedEvent event) throws IOException {
+//        if (event.getMessage().getContentRaw().startsWith("!factcheck")) {
+//            String query = event.getMessage().getContentRaw().substring("!factcheck".length()).trim();
+//            String result = run(String.valueOf(query));
+//            event.getChannel().sendMessage(result).queue();
+//        }
+//    }
 
-    public void onMessageRecieved(MessageReceivedEvent event) {
-        if (event.getMessage().getContentRaw().startsWith("!factcheck")) {
-            String query = event.getMessage().getContentRaw().substring("!factcheck".length()).trim();
-            String result = factCheck(query);
-            event.getChannel().sendMessage(result).queue();
-        }
-    }
-    private static String factCheck(String query) {
-        OkHttpClient client = new OkHttpClient();
 
+    OkHttpClient client = new OkHttpClient();
+
+    String run(String url) throws IOException {
         Request request = new Request.Builder()
-                .url("api_key")
-                .addHeader("Content-Type", "application/json")
-                .addHeader("Authorization", "Bearer YOUR_API_KEY")
-                .post(RequestBody.create(MediaType.get("application/json"), "{\"prompt\":\"" + query + "\",\"max_tokens\":10}"))
+                .url(url)
                 .build();
 
         try (Response response = client.newCall(request).execute()) {
             return response.body().string();
-        } catch (IOException e) {
-            e.printStackTrace();
-            return "An error occurred while fact checking:" + e.getMessage();
-        }
+
+    }
+
+        run("http://placekitten.com/200/300");
+//    private static String factCheck(String query) {
+//        OkHttpClient client = new OkHttpClient();
+//
+//        Request request = new Request.Builder()
+//                .url("https://api.openai.com/v1/engines/davinci/completions")
+//                .addHeader("Content-Type", "application/json")
+//                .addHeader("Authorization", "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")
+//                .get().build();
+//
+//        try (Response response = client.newCall(request).execute()) {
+//            return response.body().string();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//            return "An error occurred while fact checking:" + e.getMessage();
+//        }
     }
     }
